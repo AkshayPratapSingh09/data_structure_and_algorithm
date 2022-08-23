d91c99eb446645218cd4772fcff2eec7ae9b0875
AkshayPratapSingh09 <worklikeakshay@gmail.com>
2022-08-23 22:36:38 +0530

    Binary Tree Added

--- 11_Binary_Tree/main.py
+++ 11_Binary_Tree/main.py
@@ -7,6 +7,16 @@ class Node:
 class Tree:
     def createNode(self,data):
         return Node(data)
-a = Tree
-print(a.createNode(10,20))
-print(a.Node.value)
+# a = Tree
+# print(a.createNode(10,20))
+# print(a.Node.value)
+
+    def insert(self,node,data):
+        if Node is None:
+            return self.createNode(data)
+        if data < node.data:
+            node.left = self.insert(node.left,data)
+        else:
+            node.right = self.insert(node.right,data)
+            return Node
+
