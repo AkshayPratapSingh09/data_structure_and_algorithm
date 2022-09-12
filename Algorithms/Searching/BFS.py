e608dc7c2f2836cdecf816d0e8f52f6292f4794f
Akshay <worklikeakshay@gmail.com>
2022-09-12 23:00:26 +0530

    Added BFS

--- Algorithms/Searching/BFS.py
+++ Algorithms/Searching/BFS.py
@@ -1,33 +1,4 @@
-class treenode:
-    def __init__(self,data):
-        self.data = data
-        self.children = []
-        self.parent = None
-
-    def get_level(self):
-        level = 0
-        p = self.parent
-        while p:
-            level += 1
-            p = p.parent
-        return level
-
-    def add_child(self,child):
-        child.parent = self
-        self.children.append(child)
-
-    def print_tree(self):
-        spaces = ' ' * self.get_level() * 3
-        prefix = spaces + "|__" if self.parent else ""
-        print(prefix + self.data)
-        if self.children:
-            for child in self.children:
-                child.print_tree()
-
-def build_product_tree():
-    
-    
-    # root.print_tree()
+bfs for the sake of not being the only one in the end of the world and lets not get first of all the 
 
 
 
