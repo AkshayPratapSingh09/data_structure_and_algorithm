8b204eb53397d94a0b20f558c25f7145234d85b8
Akshay <worklikeakshay@gmail.com>
2022-10-08 23:56:58 +0530

    Added Algos

---
+++ DSA Questions/max_and_min.py
@@ -0,0 +1,11 @@
+arr = [3, 5, 4, 1, 9]
+arr.sort()
+
+print("Max Element:",arr[-1])
+print("Min Element:",arr[0])
+
+# Alternate Method::-
+
+# def min_max(arr):
+    # sort the array in any way:
+    # and print the first and last element
